39aeb65515bc474346dde9fe1c7de0b56c9a72e6
Jimmy Tran <jimmy.tran@watasolutions.com>
2020-04-25 21:35:06 +0700

    lowerCamelCase

--- api/account/form.go
+++ api/account/form.go
@@ -2,23 +2,26 @@ package account
 
 import (
 	"time"
-
-	"github.com/jinzhu/gorm"
 )
 
 // Account custom select
 type Account struct {
-	gorm.Model
-	Username  string
-	Email     string
-	FirstName string
-	LastName  string
-	Birthday  *time.Time
-	Gender    string
-	Status    string
-	LinkedFB  bool
-	LinkedTW  bool
-	LinkedGG  bool
-	CreatedBy string
-	UpdatedBy string
+	// gorm.Model
+	ID        uint       `gorm:"primary_key" json:"accountId"`
+	CreatedAt time.Time  `json:"createdAt"`
+	UpdatedAt time.Time  `json:"updatedAt"`
+	DeletedAt *time.Time `sql:"index" json:"deletedAt"`
+
+	Username  string     `json:"username"`
+	Email     string     `json:"email"`
+	FirstName string     `json:"firstName"`
+	LastName  string     `json:"lastName"`
+	Birthday  *time.Time `json:"birthday"`
+	Gender    string     `json:"gender"`
+	Status    string     `json:"status"`
+	LinkedFB  bool       `json:"linkedFB"`
+	LinkedTW  bool       `json:"linkedTW"`
+	LinkedGG  bool       `json:"linkedGG"`
+	CreatedBy string     `json:"createdBy"`
+	UpdatedBy string     `json:"updatedBy"`
 }
